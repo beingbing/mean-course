d48fd058b5a25ad76ae2f052e75313ef8d9b1453
beingbing <shaikhsamartaj@gmail.com>
2018-08-10 20:13:31 +0530

    lecture 37

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('My first middleware');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hello from the other side');
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {id: '12345', title: 'my first posts', content: 'this is the content of my first post'},
+        {id: 'sdtre',title: 'my second posts', content: 'this is the content of my second post'},
+        {id: 'uytrf',title: 'my third posts', content: 'this is the content of my third post'}
+    ];
+    
+    res.status(200).json({
+        message: 'posts fetch successfully',
+        posts: posts
+    });
 });
 
 module.exports = app;
